a359847ff48092ce0d8dfacd284feb0864f3cc35
Bruno <brunopassos_bp@hotmail.com>
2022-06-16 15:28:08 -0300

    feat/ Implementação de login

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,12 @@
 import Logo from "../../components/Logo";
 import Button from "../../components/Button";
 import { Container, Content, Header } from "./styles";
+import { useParams } from "react-router-dom";
 
 function Dashboard() {
+
+  const params = useParams();
+
   return (
     <>
       <Container>
@@ -11,8 +15,8 @@ function Dashboard() {
           <Button textColor={"--gray0"} bgColor={"--gray3"}>Sair</Button>
         </div>
         <Header>
-            <h2>Olá, Bruno Passos</h2>
-            <span>Primeiro módulo (Introduçao ao FrontEnd)</span>
+            <h2>{`Olá, ${params.id}`}</h2>
+            <span></span>
         </Header>
         <Content>
             <span>Tecnologias</span>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,48 +3,88 @@ import Logo from "../../components/Logo";
 import { Container, Content } from "./styles";
 import Button from "../../components/Button";
 import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup"
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
+import api from "../../services/api";
+import { toast } from "react-toastify";
 
 function Home() {
 
-    const schema = yup.object().shape({
-        email: yup.string().required("Campo obrigatório").email(),
-        password: yup.string().required("Campo obrigatório")
-    })
+  const schema = yup.object().shape({
+    email: yup.string().required("Campo obrigatório").email(),
+    password: yup.string().required("Campo obrigatório"),
+  });
 
-    const {register, handleSubmit, formState: { errors }} = useForm({
-        resolver: yupResolver(schema),
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    function onSubmitFunction(data){
-        console.log(data)
-    }
+  function onSubmitFunction(data) {
+    api
+      .post("/sessions", data)
+      .then((resp) => {
+        console.log(resp)
+        localStorage.setItem("token", resp.data.token);
+        localStorage.setItem("id", resp.data.user.id);
+        toast.success(`Seja bem vindo(a) ${resp.data.user.name}`)
+        return history.push(`/dashboard/${resp.data.user.name}`)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleNavigation = (path) => {
-        return history.push(path)
-      };
+  const handleNavigation = (path) => {
+    return history.push(path);
+  };
 
-    return ( 
-        <>
-            <Logo/>
-            <Container>
-                    <h2>Login</h2>
-                <Content onSubmit={handleSubmit(onSubmitFunction)}>                    
-                    <Input error={errors.email?.message} register={register} name="email" placeholderText="Digite o seu email" children="Email" type="text"/>
-                    <Input error={errors.password?.message} register={register} name="password" placeholderText="Digite a sua senha" children="Senha" type="password"/>
-                    <Button type="submit" bgColor={"--color-primary"}  >Entrar</Button>
-                </Content>
-            <div>
-                <span>Ainda não tem uma conta?</span>
-                <Button onClick={handleNavigation} path="/register" bgColor={"--gray1"} textColor={"--gray0"} >Cadastre-se</Button>                
-            </div>
-            </Container>
-        </>
-     );
+  return (
+    <>
+      <Logo />
+      <Container>
+        <h2>Login</h2>
+        <Content onSubmit={handleSubmit(onSubmitFunction)}>
+          <Input
+            error={errors.email?.message}
+            register={register}
+            name="email"
+            placeholderText="Digite o seu email"
+            children="Email"
+            type="text"
+          />
+          <Input
+            error={errors.password?.message}
+            register={register}
+            name="password"
+            placeholderText="Digite a sua senha"
+            children="Senha"
+            type="password"
+          />
+          <Button type="submit" bgColor={"--color-primary"}>
+            Entrar
+          </Button>
+        </Content>
+        <div>
+          <span>Ainda não tem uma conta?</span>
+          <Button
+            onClick={handleNavigation}
+            path="/register"
+            bgColor={"--gray1"}
+            textColor={"--gray0"}
+          >
+            Cadastre-se
+          </Button>
+        </div>
+      </Container>
+    </>
+  );
 }
 
 export default Home;
