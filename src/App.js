5c9a3d3f16792d4f8c4799cdb26e1d8df5f99c83
Bruno <brunopassos_bp@hotmail.com>
2022-06-16 14:33:15 -0300

    feat/ Implementação do cadastro de usuário

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { CreateGlobalStyle } from "./styles/global";
 import Routes from "./routes";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-      <CreateGlobalStyle/>
-      <Routes/>
+      <CreateGlobalStyle />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      {/* Same as */}
+      <ToastContainer />
+      <Routes />
     </div>
   );
 }
