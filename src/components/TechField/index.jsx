47ff989a2cef3ba66ba4145d35f26c68e1f30d00
Bruno <brunopassos_bp@hotmail.com>
2022-06-16 21:10:13 -0300

    feat/ Implementação do modal, adição de techs e estilização da dashboard

--- src/components/TechField/index.jsx
+++ src/components/TechField/index.jsx
@@ -1,10 +1,20 @@
-function TechField({techs}) {
-    return ( 
+import {AiFillEdit, AiFillDelete} from "react-icons/ai";
+import { StyledLi, StyledUl } from "./styles";
 
-        <div>                       
-            {techs.length === 0 ? <span>Clique no + e cadastre as suas tecnologias!</span> : <ul><li>{techs}</li></ul>}            
-        </div>
-     );
+function TechField({ techs }) {
+  return (
+    <div>
+      {!techs ? (
+        <span>Clique no + e cadastre as suas tecnologias!</span>
+      ) : (
+        <StyledUl>
+          {techs.map((item) => {
+            return <StyledLi key={item.id}><div><p>{item.title}</p></div><div><span>{item.status}</span></div><div><button><AiFillEdit/></button><button><AiFillDelete/></button></div></StyledLi>;
+          })}
+        </StyledUl>
+      )}
+    </div>
+  );
 }
 
 export default TechField;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import Logo from "../../components/Logo";
 import Button from "../../components/Button";
-import { Container, Content, Header } from "./styles";
+import { Container, Content, Header, StyledForm } from "./styles";
 import { useParams } from "react-router-dom";
 import api from "../../services/api";
 import { useState } from "react";
@@ -9,8 +9,18 @@ import { toast } from "react-toastify";
 import { AiFillPlusSquare } from "react-icons/ai";
 import { useEffect } from "react";
 import TechField from "../../components/TechField";
+import ReactModal from 'react-modal';
+import Input from "../../components/Input";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useForm } from "react-hook-form";
+
+
 
 function Dashboard() {
+
+  const [open, setOpen] = useState(false);  
+  const [techs, setTechs] = useState([]);
 
   const params = useParams();
 
@@ -19,19 +29,55 @@ function Dashboard() {
   useEffect(() => {
     api.get(`/users/${localStorage.getItem("id")}`).then((resp) => {
       setUserData(resp.data)
+      setTechs(resp.data.techs)
     }).catch((err) => {
       console.log(err);
     })
-  }, []);
+  }, [techs]);
 
 
   const history = useHistory();
 
   const handleNavigation = (path) => {
+    localStorage.removeItem("token");
     toast.success("Deslogado com sucesso.")
     return history.push(path);
   };
 
+  const schema = yup.object().shape({
+    title: yup.string().required("Campo obrigatório"),
+    status: yup.string().required("Campo obrigatório")
+  })
+
+  const {register, handleSubmit, formState: { errors }} = useForm({
+      resolver: yupResolver(schema),
+  });
+
+  function onSubmitFunction(data){ 
+    api
+    .post("/users/techs", data)
+    .then((_) => {
+        toast.success("Tecnologia cadastrada com sucesso!");
+        setOpen(!open);
+    })
+    .catch((err) => {
+        console.log(err)
+        toast.error("Erro ao cadastrar, tente novamente!");
+    });
+  }
+
+  const styleDiv = {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-around",
+    height: "40.11px",
+    alignItems: "center",
+    background: "var(--gray2)",
+    color: "var(--gray0)",
+    fontSize: "11.2304px",
+    fontFamily: "Inter",
+    fontWeigth: "400"   
+  }
 
   return (
     <>
@@ -46,12 +92,43 @@ function Dashboard() {
         </Header>
         <Content>
             <span>Tecnologias</span>
-            <button>
+            <button onClick={() => setOpen(!open)}>
               <AiFillPlusSquare/>
             </button>
         </Content>
-            <TechField techs={userData.techs}/>
+            <TechField techs={techs}/>
       </Container>
+      <ReactModal portalClassName="modal" isOpen={open} onRequestClose={() => setOpen(!open)} ariaHideApp={false}
+        style={{
+          content: {
+            backgroundColor: "var(--gray3)",
+            height: "300px",
+            margin: "120px auto",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "0"
+          },
+          overlay: {
+            background: "rgba(18, 18, 20, 0.5)"
+          }
+        }}
+      >        
+        <div style={styleDiv}>
+          <span>Cadastrar tecnologia</span>
+          <Button bgColor={"--gray2"} textColor={"--gray1"} onClick={() => setOpen(!open)}>x</Button>
+        </div>
+        <StyledForm onSubmit={handleSubmit(onSubmitFunction)} >
+          <Input error={errors.title?.message}  children={"Nome"} placeholderText={"JavaScript"} type={"text"} name={"title"} register={register} />
+          <label>Selecionar Status</label>
+          <select {...register("status")}>
+            <option value="Iniciante">Iniciante</option>
+            <option value="Intermediário">Intermediário</option>
+            <option value="Avançado">Avançado</option>
+          </select>
+          <Button bgColor={"--color-primary"}>Cadastrar Tecnologia</Button>
+        </StyledForm>
+      </ReactModal>
     </>
   );
 }
